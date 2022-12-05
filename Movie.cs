d89fd43a53ceb0501e780833352e3efa3fff69b9
Raymond McCarthy <x00179223@mytudublin.ie>
2022-12-05 06:42:21 +0000

    initial commit

---
+++ Movie.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json.Serialization;
+
+namespace EADCA3
+{
+    public class Movie
+    {
+        [JsonPropertyName("poster_path")]
+        public String? posterPath {get; set;}
+        public bool? adult { get; set; }
+        [JsonPropertyName("release_date")]
+        public String? releaseDate { get; set; }
+        [JsonPropertyName("genre_ids")]
+        public List<int>? genreIds { get; set; }
+        public int? id { get; set; }
+        [JsonPropertyName("original_title")]
+        public String? originalTitle { get; set; }
+        [JsonPropertyName("original_language")]
+        public String? originalLanguage { get; set; }
+        public String? title { get; set; }
+        [JsonPropertyName("backdrop_path")]
+        public String? backdropPath { get; set; }
+        public float? popularity { get; set; }
+        [JsonPropertyName("vote_count")]
+        public int? voteCount { get; set; }
+        public bool? video { get; set; }
+        [JsonPropertyName("vote_average")]
+        public float? voteAverage { get; set; }
+
+    }
+}
---
+++ PopularMoviesList.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EADCA3
+{
+    public class PopularMoviesList
+    {
+        public int? page { get; set;  }
+        public List<Movie>? results { get; set;  }
+
+        [JsonPropertyName("total_results")]
+        public int? totalResults { get; set; }
+
+        [JsonPropertyName("total_pages")]
+        public int? totalPages { get; set;  }
+
+    }
+}
